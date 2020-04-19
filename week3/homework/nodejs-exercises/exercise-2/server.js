145217772c42aab4e2fb4a52d3d3594b1bb2a5dc
okayns <55080187+okayns@users.noreply.github.com>
2020-04-19 08:17:20 +0200

    Update server.js

--- week3/homework/nodejs-exercises/exercise-2/server.js
+++ week3/homework/nodejs-exercises/exercise-2/server.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch');
 const url = 'https://restapiabasicauthe-sandbox.mxapps.io/api/books';
 //Bonus Part
 const keyword = 'admin:hvgX8KlVEa';
-const buff = new Buffer.allocUnsafe(keyword);
+const buff = new Buffer(keyword);
 const encodedKeyword = buff.toString('base64'); //(it can be also convert by using base64 node package)
 
 fetch(url, { headers: { Authorization: `Basic ${encodedKeyword}` } })
